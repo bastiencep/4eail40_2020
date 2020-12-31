c92e30ff7beba32fbea455adfe529d3cbb195dba
bastiencep <72868848+bastiencep@users.noreply.github.com>
2020-12-31 18:35:46 +0100

    Update board.go

--- exercises/chess/model/board/board.go
+++ exercises/chess/model/board/board.go
@@ -15,18 +15,40 @@ func (c *Classic) String() string {
 // PieceAt retrievves piece at give coordinates.
 // Returns nil if no piece was found.
 func (c *Classic) PieceAt(at coord.ChessCoordinates) piece.Piece {
-	panic("not implemented") // TODO: Implement
+	//panic("not implemented") // TODO: Implement
+	x, _ := at.Coord(0)
+	y, _ := at.Coord(1)
+	val := c[x][y]
+	if val != nil {
+		return val
+	} else {
+		return nil
+	}
 }
-
 // MovePiece moves a piece from given coordinates to
 // given coordinates.
 // Returns an error if destination was occupied.
 func (c *Classic) MovePiece(from coord.ChessCoordinates, to coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	//panic("not implemented") // TODO: Implement
+		if c.PieceAt(from) == nil {
+		return fmt.Errorf("No piece at %v", from.String())
+	} else {
+		x, _ := from.Coord(0)
+		y, _ := from.Coord(1)
+		p := c[x][y]
+		return c.PlacePieceAt(p, to)
+	}
 }
-
 // PlacePieceAt places a given piece at given location.
 // Returns an error if destination was occupied.
 func (c *Classic) PlacePieceAt(p piece.Piece, at coord.ChessCoordinates) error {
-	panic("not implemented") // TODO: Implement
+	//panic("not implemented") // TODO: Implement
+	if c.PieceAt(at) == nil {
+		x, _ := at.Coord(0)
+		y, _ := at.Coord(1)
+		c[x][y] = p
+		return nil
+	} else {
+		return fmt.Errorf("Destination %v already occupied", at.String())
+	}
 }

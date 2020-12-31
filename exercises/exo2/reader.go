4b7447eef81544636a896fd461aac25b79274f7e
bastiencep <72868848+bastiencep@users.noreply.github.com>
2020-12-31 18:29:38 +0100

    Update reader.go

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -10,6 +10,18 @@ type spaceEraser struct {
 	r io.Reader
 }
 
+func (tr spaceEraser) Read(buf []byte) (int, error) {
+	n, err := tr.r.Read(buf)
+	j := 0
+	for i := 0; i < n; i++ {
+		if buf[i] != 32 {
+			buf[j] = buf[i]
+			j++
+		}
+	}
+	return j, err
+}
+
 func main() {
 	s := strings.NewReader("H e l l o w o r l d!")
 	r := spaceEraser{s}
